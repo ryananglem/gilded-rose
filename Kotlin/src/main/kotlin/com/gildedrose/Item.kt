3e50cae3ea2656ed7e41c6c28a8fb7006ee1b813
Mazin Power <mazinpower@notonthehighstreet.com>
2019-01-28 15:57:25 +0000

    Fork and prep

--- Kotlin/src/main/kotlin/com/gildedrose/Item.kt
+++
@@ -1,3 +0,0 @@
-package com.gildedrose
-
-data class Item(var name: String, var sellIn: Int, var quality: Int)
---
+++ js/spec/gilded_rose_spec.js
@@ -0,0 +1,9 @@
+describe("Gilded Rose", function() {
+
+  it("should foo", function() {
+    items = [ new Item("foo", 0, 0) ];
+    update_quality();
+    expect(items[0].name).toEqual("fixme");
+  });
+
+});
