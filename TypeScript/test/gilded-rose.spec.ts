9324cdda5f1b4425c059453b43729fb7ce23873c
Ryan Anglem <ryan.anglem@gmail.com>
2019-11-10 15:14:54 +0000

    test quality degrading before sellby date

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -3,10 +3,24 @@ import { Item, GildedRose } from '../app/gilded-rose';
 
 describe('Gilded Rose', function () {
 
-    it('should foo', function() {
+    it('should add new item', function() {
         const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('foo');
+        const added = gildedRose.items[0]
+        expect(added.name).to.equal('foo');
+        expect(added.quality).to.equal(0);
+        expect(added.sellIn).to.equal(0);
     });
+
+    it('should update quality for sellin 1 day', function() {
+        const gildedRose = new GildedRose([ new Item('foo', 1, 1) ]);
+        const items = gildedRose.updateQuality();
+        const added = items[0]
+        expect(added.quality).to.equal(0);
+        expect(added.sellIn).to.equal(0);
+    });
+     
+
+
+
 
 });
